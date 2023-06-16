b29fca649d4679b775359084476d3f8cee2ac341
Argiro <elto.82@gmai.com>
2023-06-16 11:40:05 -0500

    documentation with swagger

--- src/index.js
+++ src/index.js
@@ -2,17 +2,44 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user.js");
+const path = require("path");
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerSpec = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: " NODEJS-MONGODBATLAS-API",
+      version: "1.0.0",
+      description: "A simple Express CRUD API",
+      contact: {
+        name: "Argiro Arias",
+        email: "elto.82@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:8000",
+        description: "Development server",
+      },
+    ],
+  },
+  apis: [`${path.join(__dirname, "./routes/*.js")}`],
+};
 
 const port = process.env.PORT || 9000;
 
 const app = express();
 
+//middlewares
 app.use(express.json());
 app.use("/api", userRoutes);
-
-app.get("/", (req, res) => {
-  res.status(200).json({ status: "ok", message: "Hello people" });
-});
+app.use(
+  "/api-doc",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerJsDoc(swaggerSpec))
+);
 
 //connect Db
 mongoose
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,22 +2,48 @@ const express = require("express");
 const userSchema = require("../models/user");
 
 const router = express.Router();
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the user
+ *         age:
+ *           type: number
+ *           description: The age of the user
+ *         email:
+ *           type: string
+ *           description: The email of the user
+ *       required:
+ *         - name
+ *         - age
+ *         - email
+ *       example:
+ *         name: John
+ *         age: 30
+ *         email: example@mail.com
+ */
 
-router.get("/users", (req, res) => {
-  userSchema
-    .find()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
-});
-
-router.get("/users/:id", (req, res) => {
-  const { id } = req.params;
-  userSchema
-    .findById(id)
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
-});
-
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The user was successfully created
+ */
 router.post("/users", (req, res) => {
   const user = userSchema(req.body);
   user
@@ -26,6 +52,85 @@ router.post("/users", (req, res) => {
     .catch((error) => res.json({ message: error.message }));
 });
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Return all users
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: All users successfully returned
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+router.get("/users", (req, res) => {
+  userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   get:
+ *     summary: Get a user by ID
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the user
+ *     responses:
+ *       200:
+ *         description: Successful response with the user data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: User not found
+ */
+
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   put:
+ *     summary: Update a user
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The user id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The user was successfully updated
+ *       404:
+ *         description: The user was not found
+ */
 router.put("/users/:id", (req, res) => {
   const { id } = req.params;
   const { name, age, email } = req.body;
@@ -34,7 +139,25 @@ router.put("/users/:id", (req, res) => {
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
-
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     summary: Delete a user
+ *     tags: [User]
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The user id deleted
+ *     responses:
+ *       200:
+ *         description: one user successfully deleted
+ *       404:
+ *         description: The user was not found
+ */
 router.delete("/users/:id", (req, res) => {
   const { id } = req.params;
   userSchema
